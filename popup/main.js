e6645779d1f43ebadf8aea1e81eb007b125e609b
jorgemanzo <jmanzo203689@gmail.com>
2019-10-09 12:33:48 -0700

    Added working widening and narrow controls

--- popup/main.js
+++ popup/main.js
@@ -2,15 +2,29 @@
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
+
+let notifyContentScript = async sender => {
+	let activeTab = await browser.tabs.query({active: true, currentWindow: true})
+	let activeTabId = activeTab[0].id
+	browser.tabs.sendMessage(activeTabId, {
+		sentBy: sender
+	})
+}
 let listenForClicks = () => {
-  document.getElementById("enableCheck").addEventListener("click", async (e) => {
-    // Unhide the right bar
-    let activeTab = await browser.tabs.query({active: true, currentWindow: true})
-    let activeTabId = activeTab[0].id
-    browser.tabs.sendMessage(activeTabId, {
-      message: "swap"
-    })
-  });
+	document.getElementById("enableCheck").addEventListener("click", async (e) => {
+		// Unhide the right bar
+		notifyContentScript("enableButton")
+	});
+
+	document.getElementById("narrowClick").addEventListener("click", async (e) => {
+		// Make the markdown content Wider
+		notifyContentScript("narrowButton")
+	})
+
+	document.getElementById("widenClick").addEventListener("click", async (e) => {
+		// Make the markdown content Wider
+		notifyContentScript("widenButton")
+	})
 }
 
 // Put in an event listener for the button on the plugin page.
